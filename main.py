ac843d377dd63d48939c1a67a12bded2e2682c44
Abhishek Verma <49243173+AbhishekVerma023@users.noreply.github.com>
2019-09-21 00:46:00 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from lps import computeLPSArray
+def KMPSearch(pat, txt): 
+    M = len(pat) 
+    N = len(txt) 
+    lps = [0]*M 
+    j = 0 
+    computeLPSArray(pat, M, lps) 
+  
+    i = 0 
+    while i < N: 
+        if pat[j].lower() == txt[i].lower(): 
+            i += 1
+            j += 1
+  
+        if j == M: 
+            print ("Found pattern at index " + str(i-j)) 
+            j = lps[j-1] 
+  
+        elif i < N and pat[j] != txt[i]: 
+            if j != 0: 
+                j = lps[j-1] 
+            else: 
+                i += 1
+  
